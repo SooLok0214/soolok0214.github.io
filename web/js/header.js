be369ef17493b27c10dd3f94f7a5c97d2099c245
SooLok0214 <wenglok241026@dasein.edu.my>
2026-05-10 12:59:30 +0800

    feat: updated heaedr. now have hover underline animation

--- web/js/header.js
+++ web/js/header.js
@@ -10,3 +10,22 @@ closeBtn.addEventListener('click', () => {
     menuOverlay.classList.remove('burger-active');
     document.body.style.overflow = 'auto';
 });
+
+const headings = document.querySelectorAll(".underline-animate");
+
+headings.forEach((h2) => {
+    const underline = h2.querySelector(".underline");
+    h2.addEventListener("mouseenter", () => {
+        gsap.to(underline, {
+            width: "100%",
+            backgroundColor: "var(--color-secondary-green)",
+            duration: 0.2
+        });
+    });
+    h2.addEventListener("mouseleave", () => {
+        gsap.to(underline, {
+            width: 0,
+            duration: 0.2
+        });
+    });
+});
